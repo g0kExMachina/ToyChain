c465e41f8c3c0052c1fd45ce34ceb2ecb5f968b4
Gokul Tharayil <mail.gokultp@gmail.com>
2018-06-05 23:29:47 +0530

    Corrected Syntax Error

--- blockchain.py
+++ blockchain.py
@@ -1,1 +1,33 @@
-class
+import time
+
+class block:
+	def __init__(self,index,timestamp,prev_hash,transcations):
+		self.index = index
+		self.timestamp = time.time()
+		self.prev_hash = prev_hash
+		self.transcations = transcations
+
+	def calculate_hash(self):
+		pass
+
+	def transcation_hash(self):
+		pass
+
+
+class transcations:
+
+	def __init__(self,input_addr,output_addr,value):
+		self.input = input_addr
+		self.output = output_addr
+		self.value = value
+
+class blockchain:
+
+	def __init__(self):
+		self.unconfirmed_trancation = []
+		self.chain = []
+		self.create_genesis()
+
+	def create_genesis(self):
+		pass
+
