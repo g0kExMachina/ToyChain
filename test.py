f4525038f030e55a9a05933adec9ac3f1ec1c560
Gokul Tharayil <mail.gokultp@gmail.com>
2018-06-09 11:46:38 +0530

    Added Transcation basic test

--- test.py
+++ test.py
@@ -1,4 +1,7 @@
-from blockchain import block,blockchain
+from blockchain import block,blockchain,transactions
+import uuid
+import string
+import random
 
 def check_hash():
   transaction = [{"sender":"alice","receiver":"s"}]
@@ -27,13 +30,29 @@ def print_genesis():
   print("--------------------------------------")
 
 def create_10blocks():
-  transcations = [{"sender":"alice","receiver":"sink"}]
+  transcations = make_10transactions()
   new_blockchain = blockchain()
   for i in range(10):
     new_blockchain.create_blocks(transactions=transcations)
   new_blockchain.iterate_blocks()
 
+def check_transactions():
+  new_trans = transactions("Gokul","Alice",100)
+  print(new_trans.to_dict())
+
+def make_10transactions():
+  transactions_list  =[]
+  for i in range(10):
+    sender = str(uuid.uuid4()).replace('-','')
+    receiver = str(uuid.uuid4()).replace('-','')
+    new_trans = transactions(sender,receiver,random.randint(50,5000))
+    transactions_list.append(new_trans.to_dict())
+  return transactions_list
+
   # Running Things
 
 #check_mining_genesis()
+#check_transactions()
 create_10blocks()
+#make_10transcations()
+
